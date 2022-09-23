bcaf5c44524e6df9090be79132d9f5543ef1d8ea
Your Name <you@example.com>
2022-09-23 17:13:15 +0800

    init

---
+++ README.md
@@ -0,0 +1,75 @@
+# assets plugin for webpack
+
+一个webpack插件，用来修改最终asset的文件路径或过滤文件。
+
+> NOTE: Node v10+ and webpack v4+ are supported and tested.
+
+## 关于
+
+control-output-plugin用来修改最终asset的文件路径或过滤文件，该事件发生在compiler.hooks.emit中。需要注意的一点，它不会更改模块中的依赖关系，所以使用它你模块中的依赖将发生错误。还有一点它拥有clean-webpack-plugin的部分功能，用来清除过期的asset文件。
+
+## 安装
+
+`npm install --save-dev control-output-plugin`
+
+## 使用
+
+```js
+import ControlOutputPlugin from 'control-output-plugin'
+
+const webpackConfig = {
+    plugins: [
+        new ControlOutputPlugin(),
+    ],
+};
+
+module.exports = webpackConfig;
+```
+
+## 选项
+
+```js
+new ControlOutputPlugin({
+    // 资源输出路径控制。
+    //
+    // default: []
+    control: [
+        {
+            // 过滤掉匹配的文件
+            test: /\.js|map/,
+            replace: null
+        },
+        {
+            // 将匹配的文件移动到output的img文件夹内
+            test: /\.png|jpe?g/,
+            replace: 'img/[basename]'
+        },
+        {
+            // 将匹配的文件的相对路径前再加一个font文件夹
+            test: /\.ttf|eot|woff/,
+            replace: 'font/[resolvePath]'
+        },
+        {
+            // 在匹配的文件的相对路径前再加一个css文件夹
+            test: /\.css/,
+            replace: name => `css/${name}`
+        }
+    ],
+
+    // del@4的options。
+    // See https://www.npmjs.com/package/del
+    //
+    // default: {
+    //   dot: true,
+    //   force: true,
+    //   dryRun: false,
+    //   ...
+    // }
+    delOptions: {},
+
+    // 是否提示
+    //
+    // default: true
+    verbose: true
+});
+```
---
+++ src/index.js
@@ -0,0 +1,176 @@
+import path from 'path'
+import del from 'del'
+import { validate } from 'schema-utils'
+
+const pluginName = 'control-output-plugin'
+
+const defaultOptions = {
+  additionalProperties: true,
+  properties: {
+    control: {
+      description: '资源输出路径控制',
+      type: 'array'
+    },
+    delOptions: {
+      description: 'del的options',
+      type: 'object'
+    },
+    verbose: {
+      description: '是否提示',
+      type: 'boolean'
+    }
+  },
+  type: 'object'
+}
+
+const defaultControlItemOptions = {
+  additionalProperties: true,
+  properties: {
+    test: {
+      description: '资源路径匹配',
+      anyOf: [
+        {
+          type: 'string'
+        },
+        {
+          instanceof: 'Function'
+        },
+        {
+          instanceof: 'RegExp'
+        }
+      ]
+    },
+    replace: {
+      description: '资源路径替换',
+      anyOf: [
+        {
+          type: 'null'
+        },
+        {
+          type: 'string'
+        },
+        {
+          instanceof: 'Function'
+        }
+      ]
+    }
+  },
+  type: 'object'
+}
+
+export default class ControlOutputPlugin {
+  constructor(options = {}) {
+    this.options = Object.assign({
+      control: [],
+      delOptions: {},
+      verbose: true
+    }, options)
+
+    validate(defaultOptions, this.options)
+
+    this.options.control.forEach(item => {
+      validate(defaultControlItemOptions, item)
+    })
+
+    this.outputPath = ''
+    this.currentAssets = []
+  }
+
+  apply(compiler) {
+    if (!compiler.options.output || !compiler.options.output.path) {
+      console.warn(`${pluginName}: options.output.path not defined. Plugin disabled...`)
+      return
+    }
+
+    this.outputPath = compiler.options.output.path
+
+    compiler.hooks.emit.tap(pluginName, compilation => {
+      // 判断是否编译错误
+      if (this.error(compilation.getStats())) return
+      // 替换路径
+      const assets = compilation.assets
+      const assetsInfo = compilation.assetsInfo
+      Object.keys(assets).forEach(name => {
+        const newName = this.redirect(name)
+        if (newName === name) return
+
+        if (newName) {
+          assets[newName] = assets[name]
+          assetsInfo.set(newName, assetsInfo.get(name))
+        }
+
+        delete assets[name]
+        assetsInfo.delete(name)
+      })
+    })
+
+    compiler.hooks.done.tap(pluginName, stats => {
+      // 判断是否编译错误
+      if (this.error(stats)) return
+      // 删除多余文件
+      const assetNames = Object.keys(stats.compilation.assets)
+      const removePatterns = this.currentAssets.filter(previousAsset => {
+        return assetNames.includes(previousAsset) === false
+      })
+      this.currentAssets = assetNames
+      removePatterns.length && this.remove(removePatterns)
+    })
+  }
+
+  redirect(name) {
+    return this.options.control.reduce((target, item) => {
+      return this.test(target, item.test)
+        ? this.replace(target, item.replace)
+        : target
+    }, name)
+  }
+
+  test(name, test) {
+    if (test instanceof RegExp) {
+      return test.test(name)
+    } else if (typeof test === 'function') {
+      return test(name)
+    } else if (typeof test === 'string') {
+      return ~name.indexOf(test)
+    } else {
+      return false
+    }
+  }
+
+  replace(name, replace) {
+    const basename = path.basename(name)
+    if (typeof replace === "function") {
+      return replace(name)
+    } else if (typeof replace === "string") {
+      return replace
+        .replace(/\[basename\]/g, basename)
+        .replace(/\[resolvePath\]/g, name)
+    } else {
+      return null
+    }
+  }
+
+  remove(patterns) {
+    const deleted = del.sync(patterns, Object.assign({
+      dot: true,
+      force: true,
+      dryRun: false,
+      cwd: this.outputPath,
+      ignore: this.currentAssets
+    }, this.options.delOptions))
+    
+    this.options.verbose && deleted.forEach(file => {
+      const filename = path.relative(process.cwd(), file)
+      console.warn(`${pluginName}: removed ${filename}`)
+    })
+  }
+
+  error(stats) {
+    if (stats.hasErrors()) {
+      this.options.verbose && console.warn(`${pluginName}: pausing due to webpack errors`)
+      return true
+    } else {
+      return false
+    }
+  }
+}
